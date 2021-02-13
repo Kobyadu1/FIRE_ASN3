66c18a5a72b804e556085340f5b1f977f7120a0f
Koby Adu-Bonnah <42681553+Kobyadu1@users.noreply.github.com>
2021-02-12 23:13:42 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from Bio import Entrez
+Entrez.email = "kobyadu1@gmail.com"
+from Bio import Entrez, SeqIO
+
+
+def _n_count(s):
+    return s.count('N')
+
+
+n_count = []
+data = pd.read_csv('koby.csv')
+
+for s in data['Accession']:
+    handle = Entrez.efetch(db="nucleotide", id=Entrez.read(Entrez.esearch(db="nucleotide", term=s))['IdList'][0],
+                           rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    handle.close()
+    n_count.append(_n_count(record.seq))
+
+data['n_count'] = n_count
+data.to_csv('koby_new',index=False)
